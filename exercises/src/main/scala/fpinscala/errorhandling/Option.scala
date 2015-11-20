6a6d69f964bd2fea438c32adf5c2b876a8ea64be
Andrey Tsykunov <andrey.tsykunov@gmail.com>
2015-11-19 23:46:18 -0500

    Chapter 4 - Option

--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -1,18 +1,27 @@
 package fpinscala.errorhandling
 
 
+import java.util.regex.{Pattern, PatternSyntaxException}
+
+import scala.annotation.tailrec
 import scala.{Option => _, Some => _, Either => _, _} // hide std library `Option`, `Some` and `Either`, since we are writing our own in this chapter
 
 sealed trait Option[+A] {
-  def map[B](f: A => B): Option[B] = sys.error("todo")
+  def map[B](f: A => B): Option[B] = this match {
+    case Some(x) => Some(f(x))
+    case None => None
+  }
 
-  def getOrElse[B>:A](default: => B): B = sys.error("todo")
+  def getOrElse[B>:A](default: => B): B = this match {
+    case Some(x) => x
+    case None => default
+  }
 
-  def flatMap[B](f: A => Option[B]): Option[B] = sys.error("todo")
+  def flatMap[B](f: A => Option[B]): Option[B] = map(f) getOrElse None
 
-  def orElse[B>:A](ob: => Option[B]): Option[B] = sys.error("todo")
+  def orElse[B>:A](ob: => Option[B]): Option[B] = map(Some(_)) getOrElse ob
 
-  def filter(f: A => Boolean): Option[A] = sys.error("todo")
+  def filter(f: A => Boolean): Option[A] = flatMap(a => if(f(a)) Some(a) else None)
 }
 case class Some[+A](get: A) extends Option[A]
 case object None extends Option[Nothing]
@@ -38,11 +47,41 @@ object Option {
   def mean(xs: Seq[Double]): Option[Double] =
     if (xs.isEmpty) None
     else Some(xs.sum / xs.length)
-  def variance(xs: Seq[Double]): Option[Double] = sys.error("todo")
 
-  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = sys.error("todo")
+  def variance(xs: Seq[Double]): Option[Double] = {
+    mean(xs).flatMap(m => mean(xs.map(x => math.pow(x - m, 2))))
+  }
 
-  def sequence[A](a: List[Option[A]]): Option[List[A]] = sys.error("todo")
+  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = {
+    for {
+      aValue <- a
+      bValue <- b
+    } yield f(aValue, bValue)
+  }
+
+  def pattern(s: String): Option[Pattern] =
+    try {
+      Some(Pattern.compile(s))
+    } catch {
+      case e: PatternSyntaxException => None
+    }
+
+  def mkMatcher(pat: String): Option[String => Boolean] =
+    pattern(pat) map (p => (s: String) => p.matcher(s).matches)
+
+  def bothMatch(pat: String, pat2: String, s: String): Option[Boolean] =
+    for {
+      f <- mkMatcher(pat)
+      g <- mkMatcher(pat2)
+    } yield f(s) && g(s)
+
+  def bothMatch_2(pat: String, pat2: String, s: String): Option[Boolean] =
+    map2(mkMatcher(pat), mkMatcher(pat2))(_(s) && _(s))
+
+  def sequence[A](a: List[Option[A]]): Option[List[A]] = a match {
+    case Nil => Some(Nil)
+    case x :: xs => map2(x, sequence(xs))((x, xs) => x :: xs)
+  }
 
   def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = sys.error("todo")
 }
---
+++ exercises/src/test/scala/fpinscala/errorhandling/OptionTest.scala
@@ -0,0 +1,53 @@
+package fpinscala.errorhandling
+
+import org.scalatest.{Matchers, FunSuite}
+
+class Base(val data: String)
+
+class Derived(data: String) extends Base(data)
+
+class OptionTest extends FunSuite with Matchers {
+  val x = new Derived("X")
+  val y = new Base("Y")
+
+  test("map") {
+    Some(x).map(_.data) should be (Some("X"))
+    Some(x).flatMap(x => Some(x.data)) should be (Some("X"))
+  }
+
+  test("getOrElse") {
+    Some(x).getOrElse(y) should be (x)
+    None.getOrElse(y) should be (y)
+  }
+
+  test("orElse") {
+    Some(x).orElse(Some(y)) should be (Some(x))
+    None.orElse(Some(y)) should be (Some(y))
+    Some(x).orElse(None) should be (Some(x))
+    None.orElse(None) should be (None)
+  }
+
+  test("filter") {
+    Some(x).filter(_.data != "X") should be (None)
+    Some(x).filter(_.data == "X") should be (Some(x))
+
+    val none: Option[Base] = None
+    none.filter(_.data != "X") should be (None)
+  }
+
+  test("variance") {
+    Option.variance(Seq()) should be (None)
+    Option.variance(Seq(2.0)) should be (Some(0.0))
+  }
+
+  test("map2") {
+    Option.map2(Some("a"), Some("b"))(_ + _) should be (Some("ab"))
+    Option.map2(Some("a"), None)(_ + _) should be (None)
+    Option.map2(None: Option[String], Some("a"))(_ + _) should be (None)
+  }
+  test("sequence") {
+    Option.sequence(List(Some("a"), Some("b"))) should be (Some(List("a", "b")))
+    Option.sequence(List(Some("a"), None)) should be (None)
+  }
+
+}
